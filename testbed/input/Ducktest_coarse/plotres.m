bc9c330916dd0a1af9de5c775b994bfd9c5b1e3a
roelvin <d.roelvink@unesco-ihe.org>
2009-09-28 09:11:08 +0000

    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@344 86136381-33bf-4961-916e-47ea43f679af

---
+++ testbed/input/Ducktest_coarse/plotres.m
@@ -0,0 +1,39 @@
+fid=fopen('dims.dat','r');
+nt=fread(fid,[1],'double')
+nx=fread(fid,[1],'double')
+ny=fread(fid,[1],'double')
+fclose(fid)
+fixy=fopen('xy.dat','r');
+x=fread(fid,[nx+1,ny+1],'double');
+y=fread(fid,[nx+1,ny+1],'double');
+fclose(fixy)
+figure(2);
+fid=fopen('H.dat','r');
+fiz=fopen('zs.dat','r');
+fiu=fopen('ue.dat','r');
+fiv=fopen('ve.dat','r');
+for i=1:nt;
+    f=fread(fid,[nx+1,ny+1],'double');
+    z=fread(fiz,[nx+1,ny+1],'double');
+    u=fread(fiu,[nx+1,ny+1],'double');
+    v=fread(fiv,[nx+1,ny+1],'double');
+    if i>1&mod(i,1)==0
+        title(num2str(i));
+    subplot(141);pcolor(x,y,f);shading interp;caxis([0 2]);colorbar;axis equal;title('H')
+    set(gca,'xticklabel',[0 500]);set(gca,'xtick',[0 500]);
+    subplot(142);pcolor(x,y,z);shading interp;caxis([-.5 .5]);colorbar;axis equal;title('zs')
+    set(gca,'xticklabel',[0 500]);set(gca,'xtick',[0 500]);set(gca,'yticklabel',[]);    
+    subplot(143);pcolor(x,y,u);shading interp;caxis([-1 1]);colorbar;axis equal;title('u')
+    set(gca,'xticklabel',[0 500]);set(gca,'xtick',[0 500]);set(gca,'yticklabel',[]);    
+    subplot(144);pcolor(x,y,v);shading interp;caxis([-2 2]);colorbar;axis equal;title('v');
+    set(gca,'xticklabel',[0 500]);set(gca,'xtick',[0 500]);set(gca,'yticklabel',[]);drawnow;    
+%     fname=strcat(num2str(1000+i),'.jpg')
+%     print('-djpeg',fname)
+    end
+end;
+
+fclose(fid)
+fclose(fiz)
+fclose(fiu)
+fclose(fiv)
+
