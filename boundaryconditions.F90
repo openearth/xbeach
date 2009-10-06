ac85fd90a3db8792ae21a6ab277db980acec2f7a
SiggyF <fedor.baart@deltares.nl>
2009-10-06 14:55:40 +0000

    filename fix
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@382 86136381-33bf-4961-916e-47ea43f679af

--- boundaryconditions.F90
+++ boundaryconditions.F90
@@ -99,9 +99,9 @@ if(abs(par%t-par%dt)<1.d-6) then
     bcendtime=huge(0.0d0)               ! initial assumption for instat 3,4,5,7
     if (par%instat==2) then
        if(xmaster) then
-		  call readkey('params.txt','bc_file',fname)
+	call readkey('params.txt','bc_file',fname)
           if (fname=='') then
-             fname = 'bc\gen.ezs'
+             fname = 'bc/gen.ezs'
 		  endif
 		  open( unit=7, file=fname)
        endif
@@ -131,7 +131,12 @@ if(abs(par%t-par%dt)<1.d-6) then
        par%Emean=sum(dataE)/nt
     elseif (par%instat==3) then
        if (xmaster) then
-         open( unit=7, file='bc\gen.ezs')
+	call readkey('params.txt','bc_file',fname)
+          if (fname=='') then
+             fname = 'bc/gen.ezs'
+		  endif
+		  open( unit=7, file=fname)
+
        endif
        if (xmaster) then
 6        continue
