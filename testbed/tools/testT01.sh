b074bce58f3f3135432a8a4363b1f71eee84b088
SiggyF <fedor.baart@deltares.nl>
2009-10-07 13:27:44 +0000

    missing file and test
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@388 86136381-33bf-4961-916e-47ea43f679af

---
+++ testbed/tools/testT01.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+testname=Deltaflume2006_T01
+xbeachdir=`pwd`
+# goto the testbed tree
+cd testbed
+echo cleanup old run, may not work
+rm -rf run/$testname &> /dev/null
+echo create a new run directory
+mkdir -p run/$testname
+# copy input
+cp -r input/$testname run
+# go into the test
+cd run/$testname
+echo use the default parameters
+mv params_default.txt params.txt
+# run xbeach
+$xbeachdir/xbeach > output
+# did it run succesful?
+status=$?
+if [ $status != 0 ] 
+then
+    # nope
+    # don't cleanup, so we can check the output later...
+    exit $status
+else
+    # cleanup
+    cd $xbeachdir
+    rm -r run/$testname
+fi
+    
+
