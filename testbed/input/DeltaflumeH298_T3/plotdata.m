df25b30a6583d308cdccdcc00d0a3595d369d786
thiel <jaap.vanthieldevries@deltares.nl>
2009-10-15 09:57:05 +0000

    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@409 86136381-33bf-4961-916e-47ea43f679af

--- testbed/input/DeltaflumeH298_T3/plotdata.m
+++ testbed/input/DeltaflumeH298_T3/plotdata.m
@@ -37,8 +37,8 @@ end
 hardlayer = load('hardlayer_T3.dep');
 
 %% simulation results
-nam = [{'zb'};{'H'};{'zs'};{'hh'};{'u'};{'ue'};{'urms'};{'dzav'};];
-nam2 = [{'ccg'};{'Susg'};{'Subg'};];
+nam = [{'zb'};{'H'};{'zs'};{'hh'};{'u'};{'ue'};{'v'};{'ve'};{'urms'};{'dzav'};];
+nam2 = [{'ccg'};{'Susg'};{'Subg'};{'ceqbg'};{'ceqsg'};];
 
 % dimensions
 fid = fopen('dims.dat','r'); 
@@ -86,6 +86,127 @@ end
 
 
 zhard = s.zb(:,1)-hardlayer(2,:)';
+
+%% Movieplot
+ind{1} = 1:1:nx;
+nam3 = {'dzbed'};       dim3 = [nd; length(ind{1});];
+nam4 = {'pbbed'};       dim4 = [nd; ngd; length(ind{1});];
+
+for j = 1:length(nam3)
+    temp = zeros(dim3(2,j),dim3(1,j),nt);
+    fid = fopen([nam3{j},'.dat'],'r');
+    for i=1:nt
+        for ii=1:dim3(1,j)
+            ttemp = fread(fid,[nx,ny],'double');
+            temp(:,ii,i) = ttemp(ind{j},2);               
+        end
+    end
+    fclose(fid);
+    s.(nam3{j}) = temp;
+end
+
+for j = 1:length(nam4)
+    temp = zeros(dim4(3,j),dim4(1,j),dim4(2,j),nt);
+    fid = fopen([nam4{j},'.dat'],'r');
+    for i=1:nt
+        for ii=1:dim4(2,j)
+            for jj=1:dim4(1,j)
+                ttemp = fread(fid,[nx,ny],'double');
+                temp(:,jj,ii,i) = ttemp(ind{j},2);    
+            end
+        end
+    end
+    fclose(fid);
+    s.(nam4{j}) = temp;
+end
+
+figure;
+ca=[-0.2 1.2];
+cmap=colormap;
+c1r=interp1(ca,[1 length(cmap)],1);
+c1=cmap(round(c1r),:);
+c0r=interp1(ca,[1 length(cmap)],0);
+c0=cmap(round(c0r),:);
+i=1;
+temp = zeros(length(xw),nd+1);
+temp(:,2:end) = cumsum(s.dzbed(:,:,i),2);
+zlayer = repmat(s.zb(:,i),1,nd+1)-temp;
+xt = repmat(xw(:,2),1,nd+1);
+temp(:,2:end) = s.pbbed(:,:,1,i);
+temp(:,1) = temp(:,2);
+p1=    pcolor(xt,zlayer,temp); view(2); colorbar; caxis(ca); hold on; 
+p2=    plot(xw,s.zb(:,1),'k--','LineWidth',1.5); 
+p3=    plot(xw,s.zb(:,i),'k-','LineWidth',1.5);
+p4=    plot(xw,s.zs(:,i),'b','LineWidth',1.5);
+p5=    plot(xw,2650*s.ccg(:,1,i),'color',c1,'LineWidth',1.5);
+p6=    plot(xw,2650*s.ccg(:,2,i),'color',c0,'LineWidth',1.5);
+p7=    plot(xw,s.H(:,i),'c','LineWidth',1.5);
+p8=    plot(xw,s.ue(:,i),'g','LineWidth',1.5);
+axis([100 220 -4 7]);
+for i =250:nt
+    temp = zeros(length(xw),nd+1);
+    temp(:,2:end) = cumsum(s.dzbed(:,:,i),2);
+    zlayer = repmat(s.zb(:,i),1,nd+1)-temp;
+    temp(:,2:end) = s.pbbed(:,:,1,i);
+    temp(:,1) = temp(:,2);
+    set(p1,'ydata',zlayer,'cdata',temp);
+    % set(p1,'ydata',zlayer(:),'cdata',temp(:));
+    set(p3,'ydata',s.zb(:,i));
+    set(p4,'ydata',s.zs(:,i));
+    set(p5,'ydata',2650*s.ccg(:,1,i));
+    set(p6,'ydata',2650*s.ccg(:,2,i));
+    set(p7,'ydata',s.H(:,i));
+    set(p8,'ydata',s.ue(:,i));
+    title(num2str(i));
+    pause(0.1); hold off;    
+end
+
+
+%% continuity: RETE STOER!
+morfac = 10;
+morstart = 1000;
+tint = 100;
+ncorr = morstart/tint+1;
+dt = 1;
+ind = 10;
+xu(1:nx-1) = 0.5d0*(xw(1:nx-1,2)+xw(2:nx,2));
+dx = xu(ind:end)-xu(ind-1:end-1);
+Vstart = sum(dx'.*(s.zb(ind:end-1,1)-min(s.zb(ind:end-1,1))));
+% step 1: integrate profiles over flume length as function of time....
+DVbed = sum(repmat(dx,nt,1)'.*s.zb(ind:end-1,:),1)-sum(dx'.*s.zb(ind:end-1,1),1);
+plot(ts,DVbed/Vstart); hold on;
+% step 2: computre sediment volume in water column over time....
+Vsus = -sum(repmat(dx,nt,1)'.*squeeze(sum(s.ccg(ind:end-1,:,:),2)).*s.hh(ind:end-1,:)/0.6,1);
+plot(ts,(Vsus-Vsus(1))*morfac/Vstart,'r');
+% step 3: compute sediment volume that leaves the model through the
+% offshore boundary....
+DVbound = tint*squeeze(cumsum((sum(s.Susg(ind,:,:)+s.Subg(ind,:,:),2))/0.6,3));
+% step 4: correct for sediemnt suspension before t=morstart
+Vsuscorr = (Vsus-Vsus(ncorr))*morfac;
+Vsuscorr(1:ncorr) = 0;
+plot(ts,DVbound/Vstart/0.6,'g--');
+% step 5: check continuity
+plot(ts,(DVbed-Vsuscorr-DVbound')/Vstart,'k','LineWidth',1.5);
+
+%%
+figure;
+borders = [0 100 150 221];
+subplot(311); pcolor(squeeze(s.ccg(:,1,:))); axis(borders); caxis([0 0.01]); colorbar;
+subplot(312); pcolor(squeeze(s.ceqsg(:,1,:))); axis(borders); caxis([0 0.01]); colorbar;
+subplot(313); pcolor(squeeze(s.ceqbg(:,1,:))); axis(borders); caxis([0 0.01]); colorbar;
+
+figure;
+subplot(211); pcolor(squeeze(s.ue)); axis(borders); caxis([-4 4]); colorbar;
+subplot(212); pcolor(squeeze(s.ve)); axis(borders); caxis([-4 4]); colorbar;
+
+figure;
+subplot(211); pcolor(squeeze(s.u)); axis(borders); caxis([-6 6]); colorbar;
+subplot(212); pcolor(squeeze(s.v)); axis(borders); caxis([-4 4]); colorbar;
+
+figure
+DZ = diff(s.zb');
+pcolor(DZ'); caxis([-1 1]); colorbar; axis(borders); %
+
 
 %% profile evolution
 ind = round([0 0.6 1.3 3.5 9.6]*3600/par.morfac/par.tint); ind(1) = 1;
@@ -146,21 +267,5 @@ xlabel('x [m]'); ylabel('S_{mean} [m^3/m/s]');
 
 % axis([165 max(max(xw)) -4E-4 1E-4]);
 
-
-
-%movieplot
-% figure;
-% for i = 1:nt
-%    plot(xw,s.zb(:,1),'k--'); hold on;
-%    plot(xw,s.zb(:,i),'k-s');
-%    plot(xw,s.zs(:,i),'b');
-%    plot(xw,s.H(:,i),'g');
-%    plot(xw,s.ue(:,i),'r');
-%    plot(xw,par.rhos*s.ccg(:,i),'m','LineWidth',1.5);
-%    plot([0 230],[par.topr par.topr],'k--');
-%    axis([0 230 -4.5 2]);
-%    title(num2str(i));
-%    pause(0.1); hold off;
-% end
 
 end
