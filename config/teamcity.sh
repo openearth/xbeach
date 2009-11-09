06af0fd538c6418a5ffd52880e94e406c0137b89
SiggyF <fedor.baart@deltares.nl>
2009-11-09 09:40:12 +0000

    Added testsuite
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@538 86136381-33bf-4961-916e-47ea43f679af

--- config/teamcity.sh
+++ config/teamcity.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 export TEAMCITY=yes
+# we could update the configure script
+#autoreconf
 ./configure --without-mpi
 make
+echo "##teamcity[testSuiteStarted name='nompi']"
 make check
+echo "##teamcity[testSuiteFinished name='nompi']"
 make dist
 mkdir nompi
 mv ./xbeach ./*.tar.gz nompi
 make clean
 ./configure --with-mpi
 make
+echo "##teamcity[testSuiteStarted name='nompi']"
 make check
+echo "##teamcity[testSuiteFinished name='nompi']"
 make dist
 mkdir mpi
 mv ./xbeach ./*.tar.gz nompi
