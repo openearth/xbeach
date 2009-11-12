8339224afbafac21331b23e08b70ed547396d9f7
roelvin <d.roelvink@unesco-ihe.org>
2009-11-12 15:06:03 +0000

    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@597 86136381-33bf-4961-916e-47ea43f679af

--- timestep.F90
+++ timestep.F90
@@ -75,8 +75,8 @@ if(par%t>=par%tnext) then
     par%dt=par%dt-(par%t-par%tnext)
     par%t=par%tnext
     it=it+1
-!    write(*,*)'time (s) = ',par%t,' dt (s) = ',par%dt,' output step ',it
-!else
+    write(*,*)'time (s) = ',par%t,' dt (s) = ',par%dt,' output step ',it
+else
 !    write(*,*)'time (s) = ',par%t,' dt (s) = ',par%dt
 end if  
 
