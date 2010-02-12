a16925086cd96514f98eb5d3ef3605a3ecc3f1b0
hoonhout <bas@hoonhout.com>
2010-02-12 08:21:33 +0000

    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@749 86136381-33bf-4961-916e-47ea43f679af

--- config/teamcity.bat
+++ config/teamcity.bat
@@ -1,2 +1,2 @@
-echo %1 > ../version.dat
+echo Build_Revision = '%env.BUILD_VCS_NUMBER%' //  ' Mixed revisions' // 'modifications  M' > ../version.dat
 "%VS90COMNTOOLS%..\IDE\devenv.exe" /rebuild "Release|Win32" XBeach.sln
