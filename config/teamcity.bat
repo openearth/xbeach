fc2d03b0399deb524ead8e051e74b8a05c4b0b97
hoonhout <bas@hoonhout.com>
2010-02-23 09:27:08 +0000

    Updated executable revision retrieval in teamcity batch
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@806 86136381-33bf-4961-916e-47ea43f679af

--- config/teamcity.bat
+++ config/teamcity.bat
@@ -1,2 +1,2 @@
-echo Build_Revision = '%env.BUILD_VCS_NUMBER%' //  ' Mixed revisions' // 'modifications  M' > ../version.dat
+echo Build_Revision = '%env.BUILD_VCS_NUMBER%' > ../version.dat
 "%VS90COMNTOOLS%..\IDE\devenv.exe" /rebuild "Release|Win32" XBeach.sln
