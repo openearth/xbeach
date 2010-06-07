fedf3ed6c8b4fd8115d63eee72846398ad969bb8
hoonhout <bas@hoonhout.com>
2010-06-07 09:03:01 +0000

    Added Build_Date and Build_URL to dynamic version.dat
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@1115 86136381-33bf-4961-916e-47ea43f679af

--- config/teamcity.bat
+++ config/teamcity.bat
@@ -1,2 +1,5 @@
-echo Build_Revision = '%1' > ../version.dat
-"%VS90COMNTOOLS%..\IDE\devenv.exe" /rebuild "%2|%3" XBeach.sln
+echo Build_Revision = '%3' > ../version.dat
+echo Build_Date     = '%date% %time%' >> ../version.dat
+echo Build_URL      = '%4' >> ../version.dat
+
+"%VS90COMNTOOLS%..\IDE\devenv.exe" /rebuild "%1|%2" XBeach.sln
