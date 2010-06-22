2fdd17ddaf6fe22d092977d65f61cfab2044bb50
hoonhout <bas@hoonhout.com>
2010-06-22 13:27:03 +0000

    Added TeamCity interaction in teamcity.bat Fixed bug in makespectrum2.m Fixed latex formula in Delilah texts Fixed minor bug in report.py concerning runtypes
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@1186 86136381-33bf-4961-916e-47ea43f679af

--- config/teamcity.bat
+++ config/teamcity.bat
@@ -1,5 +1,15 @@
+@echo off
+
+echo ##teamcity[progressStart 'Build started']
+
 echo Build_Revision = '%3' > ../version.dat
 echo Build_Date     = '%date% %time%' >> ../version.dat
 echo Build_URL      = '%4' >> ../version.dat
 
 "%VS90COMNTOOLS%..\IDE\devenv.exe" /rebuild "%1|%2" XBeach.sln
+
+if ERRORLEVEL 1 echo ##teamcity[buildStatus status='FAILED'] else echo ##teamcity[buildStatus status='SUCCES']
+
+echo ##teamcity[progressStart 'Build finished']
+
+exit %ERRORLEVEL%
