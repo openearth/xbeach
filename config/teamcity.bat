c5978b74bfade29a29e5c69f7521123eeeac6ff4
hoonhout <bas@hoonhout.com>
2010-06-22 13:30:13 +0000

    Testing tc interaction...
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@1187 86136381-33bf-4961-916e-47ea43f679af

--- config/teamcity.bat
+++ config/teamcity.bat
@@ -6,7 +6,7 @@ echo Build_Revision = '%3' > ../version.dat
 echo Build_Date     = '%date% %time%' >> ../version.dat
 echo Build_URL      = '%4' >> ../version.dat
 
-"%VS90COMNTOOLS%..\IDE\devenv.exe" /rebuild "%1|%2" XBeach.sln
+"%VS90COMNTOOLS%..\IDE\devenv.exe" /rebuild "%1|%2" xXBeach.sln
 
 if ERRORLEVEL 1 echo ##teamcity[buildStatus status='FAILED'] else echo ##teamcity[buildStatus status='SUCCES']
 
