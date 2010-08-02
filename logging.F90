57c82b197064a6c14d53a5851293e239b0c24bf3
mccall <robert.mccall@deltares.nl>
2010-08-02 12:15:02 +0000

    display message size increased to incorporate 256 length input character strings
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@1286 86136381-33bf-4961-916e-47ea43f679af

--- logging.F90
+++ logging.F90
@@ -130,7 +130,7 @@ subroutine writelog_a(destination,form,message_char)
    implicit none
    character(*),intent(in)    ::  form,message_char
    character(*),intent(in)       ::  destination
-   character(256)             ::  display
+   character(1024)            ::  display
 
    if (form=='') then
       write(display,*)message_char
@@ -149,7 +149,7 @@ subroutine writelog_aa(destination,form,message_char1,message_char2)
    implicit none
    character(*),intent(in)    ::  form,message_char1,message_char2
    character(*),intent(in)       ::  destination
-   character(256)             ::  display
+   character(1024)            ::  display
    
    if (form=='') then
       write(display,*)message_char1,message_char2
@@ -169,7 +169,7 @@ subroutine writelog_ai(destination,form,message_char,message_int)
    character(*),intent(in)    ::  form,message_char
    character(*),intent(in)    ::  destination
    integer,intent(in)         ::  message_int
-   character(256)             ::  display
+   character(1024)            ::  display
 
    if (form=='') then
       write(display,*)message_char,message_int
@@ -189,7 +189,7 @@ subroutine writelog_ia(destination,form,mint1,mchar1)
    character(*),intent(in)    ::  form,mchar1
    character(*),intent(in)    ::  destination
    integer,intent(in)         ::  mint1
-   character(256)             ::  display
+   character(1024)            ::  display
 
    if (form=='') then
       write(display,*)mint1,mchar1
@@ -208,7 +208,7 @@ subroutine writelog_aaa(destination,form,message_char1,message_char2,message_cha
    implicit none
    character(*),intent(in)    ::  form,message_char1,message_char2,message_char3
    character(*),intent(in)       ::  destination
-   character(256)             ::  display
+   character(1024)            ::  display
    
    if (form=='') then
       write(display,*)message_char1,message_char2,message_char3
@@ -227,7 +227,7 @@ subroutine writelog_aaaa(destination,form,message_char1,message_char2,message_ch
    implicit none
    character(*),intent(in)    ::  form,message_char1,message_char2,message_char3,message_char4
    character(*),intent(in)       ::  destination
-   character(256)             ::  display
+   character(1024)            ::  display
    
    if (form=='') then
       write(display,*)message_char1,message_char2,message_char3,message_char4
@@ -248,7 +248,7 @@ subroutine writelog_aai(destination,form,message_char1,message_char2,message_int
    character(*),intent(in)    ::  form,message_char1,message_char2
    character(*),intent(in)    ::  destination
    integer,intent(in)         ::  message_int
-   character(256)             ::  display
+   character(1024)            ::  display
 
    if (form=='') then
       write(display,*)message_char1,message_char2,message_int
@@ -268,7 +268,7 @@ implicit none
    character(*),intent(in)    ::  form,message_char1
    character(*),intent(in)    ::  destination
    integer,intent(in)         ::  message_int1,message_int2
-   character(256)             ::  display
+   character(1024)            ::  display
 
    if (form=='') then
       write(display,*)message_char1,message_int1,message_int2
@@ -288,7 +288,7 @@ subroutine writelog_aia(destination,form,message_char1b,message_intb,message_cha
    character(*),intent(in)    ::  form,message_char1b,message_char2b
    character(*),intent(in)    ::  destination
    integer,intent(in)         ::  message_intb
-   character(256)             ::  display
+   character(1024)            ::  display
 
    if (form=='') then
       write(display,*)message_char1b,message_intb,message_char2b
@@ -308,7 +308,7 @@ subroutine writelog_aaai(destination,form,message_char1,message_char2,message_ch
    character(*),intent(in)    ::  form,message_char1,message_char2,message_char3
    character(*),intent(in)    ::  destination
    integer,intent(in)         ::  message_int
-   character(256)             ::  display
+   character(1024)            ::  display
 
    if (form=='') then
       write(display,*)message_char1,message_char2,message_char3,message_int
@@ -328,7 +328,7 @@ subroutine writelog_aaia(destinationb,formb,message_char1b,message_char2b,messag
    character(*),intent(in)    ::  formb,message_char1b,message_char2b,message_char3b
    character(*),intent(in)    ::  destinationb
    integer,intent(in)         ::  message_int
-   character(256)             ::  display
+   character(1024)            ::  display
 
    if (formb=='') then
       write(display,*)message_char1b,message_char2b,message_int,message_char3b
@@ -348,7 +348,7 @@ subroutine writelog_aiaa(destination,form,message_char1b,message_intb,message_ch
    character(*),intent(in)    ::  form,message_char1b,message_char2b,message_char3b
    character(*),intent(in)       ::  destination
    integer,intent(in)         ::  message_intb
-   character(256)             ::  display
+   character(1024)            ::  display
 
    if (form=='') then
       write(display,*)message_char1b,message_intb,message_char2b,message_char3b
@@ -368,7 +368,7 @@ subroutine writelog_aiaaa(destination,form,message_char1b,message_intb,message_c
    character(*),intent(in)    ::  form,message_char1b,message_char2b,message_char3b,message_char4b
    character(*),intent(in)       ::  destination
    integer,intent(in)         ::  message_intb
-   character(256)             ::  display
+   character(1024)            ::  display
 
    if (form=='') then
       write(display,*)message_char1b,message_intb,message_char2b,message_char3b,message_char4b
@@ -389,7 +389,7 @@ subroutine writelog_aiai(destination,form,message_char1,message_int1,message_cha
    character(*),intent(in)    ::  form,message_char1,message_char2
    character(*),intent(in)       ::  destination
    integer,intent(in)         ::  message_int1,message_int2
-   character(256)             ::  display
+   character(1024)            ::  display
 
    if (form=='') then
       write(display,*)message_char1,message_int1,message_char2,message_int2
@@ -409,7 +409,7 @@ subroutine writelog_aiaia(destination,form,mc1,mi1,mc2,mi2,mc3)
    character(*),intent(in)    ::  form,mc1,mc2,mc3
    character(*),intent(in)       ::  destination
    integer,intent(in)         ::  mi1,mi2
-   character(256)             ::  display
+   character(1024)            ::  display
 
    if (form=='') then
       write(display,*)mc1,mi1,mc2,mi2,mc3
@@ -429,7 +429,7 @@ subroutine writelog_aaaiai(destination,form,message_char1,message_char2,message_
    character(*),intent(in)    ::  form,message_char1,message_char2,message_char3,message_char4
    character(*),intent(in)       ::  destination
    integer*4,intent(in)          ::  message_i1,message_i2
-   character(256)             ::  display
+   character(1024)            ::  display
  
    if (form=='') then
       write(display,*)message_char1,message_char2,message_char3,message_i1,message_char4,message_i2
@@ -450,7 +450,7 @@ subroutine writelog_aiafaf(destination,form,mc1,mi1,mc2,mf1,mc3,mf2)
    character(*),intent(in)       ::  destination
    integer*4,intent(in)          ::  mi1
    real*8,intent(in)             ::  mf1,mf2
-   character(256)             ::  display
+   character(1024)            ::  display
  
    if (form=='') then
       write(display,*)mc1,mi1,mc2,mf1,mc3,mf2
@@ -470,7 +470,7 @@ implicit none
    character(*),intent(in)    ::  form,message_char1,message_char2,message_char3
    character(*),intent(in)    ::  destination
    integer*4,intent(in)       ::  message_i1,message_i2,message_i3
-   character(256)             ::  display
+   character(1024)            ::  display
  
    if (form=='') then
       write(display,*)message_char1,message_i1,message_char2,message_i2,message_char3,message_i3
@@ -491,7 +491,7 @@ implicit none
    character(*),intent(in)    ::  form,message_char1,message_char2,message_char3,message_char4
    character(*),intent(in)    ::  destination
    integer*4,intent(in)       ::  message_i1,message_i2,message_i3
-   character(256)             ::  display
+   character(1024)            ::  display
  
    if (form=='') then
       write(display,*)message_char1,message_i1,message_char2,message_i2,message_char3,message_i3,message_char4
@@ -512,7 +512,7 @@ implicit none
    character(*),intent(in)    ::  destination
    integer*4,intent(in)       ::  mi1,mi2
    real*8,intent(in)          ::  mf1
-   character(256)             ::  display
+   character(1024)            ::  display
  
    if (form=='') then
       write(display,*)mc1,mi1,mc2,mi2,mc3,mf1
@@ -533,7 +533,7 @@ implicit none
    character(*),intent(in)    ::  destination
    integer*4,intent(in)       ::  mi1,mi2
    real*8,intent(in)          ::  mf1
-   character(256)             ::  display
+   character(1024)            ::  display
  
    if (form=='') then
       write(display,*)mc1,mi1,mc2,mi2,mc3,mf1,mc4
@@ -553,7 +553,7 @@ implicit none
    character(*),intent(in)    ::  form
    character(*),intent(in)    ::  destination
    integer*4,intent(in)       ::  mi1,mi2,mi3,mi4,mi5
-   character(256)             ::  display
+   character(1024)            ::  display
  
    if (form=='') then
       write(display,*)mi1,mi2,mi3,mi4,mi5
@@ -574,7 +574,7 @@ implicit none
    character(*),intent(in)    ::  form, message_char1
    character(*),intent(in)    ::  destination
    real*8,intent(in)          ::  message_f1
-   character(256)             ::  display
+   character(1024)            ::  display
  
    if (form=='') then
       write(display,*)message_char1,message_f1
@@ -594,7 +594,7 @@ subroutine writelog_aaf(destination,form,message_char1,message_char2,message_f1)
    character(*),intent(in)    ::  form,message_char1,message_char2
    character(*),intent(in)       ::  destination
    real*8,intent(in)          ::  message_f1
-   character(256)             ::  display
+   character(1024)            ::  display
  
    if (form=='') then
       write(display,*)message_char1,message_char2,message_f1
@@ -614,7 +614,7 @@ subroutine writelog_afa(destination,form,mc1,mf1,mc2)
    character(*),intent(in)    ::  form,mc1,mc2
    character(*),intent(in)       ::  destination
    real*8,intent(in)          ::  mf1
-   character(256)             ::  display
+   character(1024)            ::  display
  
    if (form=='') then
       write(display,*)mc1,mf1,mc2
@@ -634,7 +634,7 @@ subroutine writelog_afaf(destination,form,message_char1,message_f1,message_char2
    character(*),intent(in)    ::  form,message_char1,message_char2
    character(*),intent(in)       ::  destination
    real*8,intent(in)          ::  message_f1,message_f2
-   character(256)             ::  display
+   character(1024)            ::  display
  
    if (form=='') then
       write(display,*)message_char1,message_f1,message_char2,message_f2
@@ -655,7 +655,7 @@ subroutine writelog_aaaf(destination,form,message_char1,message_char2,message_ch
    character(*),intent(in)    ::  form,message_char1,message_char2,message_char3
    character(*),intent(in)       ::  destination
    real*8,intent(in)          ::  message_f1
-   character(256)             ::  display
+   character(1024)            ::  display
  
    if (form=='') then
       write(display,*)message_char1,message_char2,message_char3,message_f1
@@ -675,7 +675,7 @@ subroutine writelog_aafa(destination,form,message_char1b,message_char2b,message_
    character(*),intent(in)    ::  form,message_char1b,message_char2b,message_char3b
    character(*),intent(in)       ::  destination
    real*8,intent(in)          ::  message_f1b
-   character(256)             ::  display
+   character(1024)            ::  display
  
    if (form=='') then
       write(display,*)message_char1b,message_char2b,message_f1b,message_char3b
@@ -695,7 +695,7 @@ subroutine writelog_afaaa(destination,form,mc1a,mfa,mc2a,mc3a,mc4a)
    character(*),intent(in)    ::  form,mc1a,mc2a,mc3a,mc4a
    character(*),intent(in)       ::  destination
    real*8,intent(in)          ::  mfa
-   character(256)             ::  display
+   character(1024)            ::  display
  
    if (form=='') then
       write(display,*)mc1a,mfa,mc2a,mc3a,mc4a
@@ -715,7 +715,7 @@ subroutine writelog_aafaf(destination,form,message_char1,message_char2,message_f
    character(*),intent(in)    ::  form,message_char1,message_char2,message_char3
    character(*),intent(in)       ::  destination
    real*8,intent(in)          ::  message_f1,message_f2
-   character(256)             ::  display
+   character(1024)            ::  display
  
    if (form=='') then
       write(display,*)message_char1,message_char2,message_f1,message_char3,message_f2
@@ -735,7 +735,7 @@ subroutine writelog_aaafaf(destination,form,message_char1,message_char2,message_
    character(*),intent(in)    ::  form,message_char1,message_char2,message_char3,message_char4
    character(*),intent(in)       ::  destination
    real*8,intent(in)          ::  message_f1,message_f2
-   character(256)             ::  display
+   character(1024)            ::  display
  
    if (form=='') then
       write(display,*)message_char1,message_char2,message_char3,message_f1,message_char4,message_f2
@@ -755,7 +755,7 @@ subroutine writelog_afafafaf(destination,form,mc1,mf1,mc2,mf2,mc3,mf3,mc4,mf4)
    character(*),intent(in)    ::  form,mc1,mc2,mc3,mc4
    character(*),intent(in)    ::  destination
    real*8,intent(in)          ::  mf1,mf2,mf3,mf4
-   character(256)             ::  display
+   character(1024)            ::  display
  
    if (form=='') then
       write(display,*)mc1,mf1,mc2,mf2,mc3,mf3,mc4,mf4
@@ -776,7 +776,7 @@ implicit none
    character(*),intent(in)    ::  destination
    integer*4,intent(in)       ::  mi1
    logical,intent(in)         ::  ml1,ml2,ml3,ml4
-   character(256)             ::  display
+   character(1024)            ::  display
  
    if (form=='') then
       write(display,*)mi1,ml1,ml2,ml3,ml4
