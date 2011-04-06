8b92b83fdb3924d39b0ef874362a7df2012a79e7
thiel <jaap.vanthieldevries@deltares.nl>
2011-04-06 11:36:00 +0000

    line too long2
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@1927 86136381-33bf-4961-916e-47ea43f679af

--- varoutput.F90
+++ varoutput.F90
@@ -464,7 +464,8 @@ contains
                       elseif (trim(par%meanvars(i))=='urms') then       ! urms
                          write(indextomeanunit(i),rec=itm)sqrt(meansparsglobal(i)%variancesquareterm2d)
                       elseif (trim(par%meanvars(i))=='thetamean') then       ! thetamean
-                         write(indextomeanunit(i),rec=itm)atan2(int(meansparsglobal(i)%mean2d)/1e9-1,mod(meansparsglobal(i)%mean2d, 1.d0)*1e3-1)
+                         write(indextomeanunit(i),rec=itm)atan2(int(meansparsglobal(i)%mean2d)/1e9-1, &
+                                                                mod(meansparsglobal(i)%mean2d, 1.d0)*1e3-1)
                       else                                                    ! non-rms variables
                          write(indextomeanunit(i),rec=itm)meansparsglobal(i)%mean2d
                       endif
