e6dfae2b688eab2d133ce372fc0279c3058b5187
mccall <robert.mccall@deltares.nl>
2011-04-18 09:50:18 +0000

    hmm, change in MSDOS? Now accepts local path
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@1948 86136381-33bf-4961-916e-47ea43f679af

--- makeversiondat.bat
+++ makeversiondat.bat
@@ -1,4 +1,4 @@
 del xbeach.ncb
-"c:\program files\tortoiseSVN\bin\SubWCRev.exe" %cd% svn_version.tpl version.dat 
+"c:\program files\tortoiseSVN\bin\SubWCRev.exe" "%cd%" svn_version.tpl version.dat 
 REM echo.>>version.dat
 REM echo Build_Type     = %1>>version.dat
