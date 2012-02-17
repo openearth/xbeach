6a1492942ab081f731efca984fa3b6fef32ad7bc
geer <pieter.vangeer@deltares.nl>
2012-02-17 15:57:40 +0000

    copy dlls to bin dir (first step to debugging netcdf configuration)
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@2657 86136381-33bf-4961-916e-47ea43f679af

--- src/xbeach/build/distribute.bat
+++ src/xbeach/build/distribute.bat
@@ -17,6 +17,9 @@ copy %3\*.dll %1\dist\win32\%2\
 rem copy additional dll files from static output folder
 copy %1\src\xbeachlibrary\bin\static\%2\*.dll %1\dist\win32\%2\
 
+rem copy additional dll files from static to bin folder
+copy %1\src\xbeachlibrary\bin\static\%2\*.dll %3\
+
 rem copy current manual
 copy %1\doc\manual\xbeach_manual.pdf %1\dist\win32\%2\
 
