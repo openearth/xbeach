09a8f25bd1a62db25561504566319d8839b6b1b5
geer <pieter.vangeer@deltares.nl>
2012-02-20 15:40:32 +0000

    automatically find devenv.exe
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@2693 86136381-33bf-4961-916e-47ea43f679af

--- config/teamcity.bat
+++ config/teamcity.bat
@@ -5,12 +5,17 @@ echo solution file	: %2
 echo Configuration name	: %3
 echo Project name	: xbeachlibrary_test
 
+set devenv_path="c:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE"
+IF EXIST "C:\Program Files (x86)\" set devenv_path="c:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE"
+
+echo devenv		: %devenv_path%
+
 echo on
 echo clean build
-"c:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" "%1\%2" /Clean %3 /Project xbeachlibrary_test
+%devenv_path%\devenv.exe "%1\%2" /Clean %3 /Project xbeachlibrary_test
 
 echo build xbech test project
-"c:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" "%1\%2" /Build %3 /Project xbeachlibrary_test
+%devenv_path%\devenv.exe "%1\%2" /Build %3 /Project xbeachlibrary_test
 
 echo run tests
 %1\test\xbeachlibrary_test\bin\%3\xbeachlibrary_test.exe
