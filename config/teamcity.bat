b6723a8f057f723df61debb3124bae28dc3f0a5c
geer <pieter.vangeer@deltares.nl>
2012-02-21 08:10:18 +0000

    more feedback
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@2697 86136381-33bf-4961-916e-47ea43f679af

--- config/teamcity.bat
+++ config/teamcity.bat
@@ -10,12 +10,31 @@ IF EXIST "C:\Program Files (x86)\" set devenv_path="c:\Program Files (x86)\Micro
 
 echo devenv		: %devenv_path%
 
+del build.log
+call "c:\Program Files\Microsoft Visual Studio 9.0\Common7\Tools\vsvars32.bat"
+
 echo on
 echo clean build
-%devenv_path%\devenv.exe "%1\%2" /Clean "%3|Win32" /Project xbeachlibrary_test
+%devenv_path%\devenv.exe "%1\%2" /Clean "%3|Win32" /Project xbeachlibrary_test /Out build.log
 
-echo build xbech test project
-%devenv_path%\devenv.exe "%1\%2" /Build "%3|Win32" /Project xbeachlibrary_test
+IF %ERRORLEVEL% == 0 GOTO BUILD
+type build.log
+echo ##teamcity[buildStatus status='FAILURE' text='{build.status.text} in cleanup, check build log']
+EXIT /B 1
+
+:BUILD
+del build.log
+
+echo ===build xbeach test project===
+%devenv_path%\devenv.exe "%1\%2" /Build "%3|Win32" /Project xbeachlibrary_test /Out build.log
+
+IF %ERRORLEVEL% == 0 GOTO NEXT
+type build.log
+echo ##teamcity[buildStatus status='FAILURE' text='{build.status.text} in compilation, check build log']
+EXIT /B 1
+
+:NEXT
+type build.log 
 
 echo run tests
 %1\test\xbeachlibrary_test\bin\%3\xbeachlibrary_test.exe
