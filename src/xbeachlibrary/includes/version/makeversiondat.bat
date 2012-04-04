6a7f792be97e33210f4cb799bedd6e633dd4484c
hoonhout <bas@hoonhout.com>
2012-04-04 12:23:58 +0000

    read revision info from environment in teamcity
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@2775 86136381-33bf-4961-916e-47ea43f679af

--- src/xbeachlibrary/includes/version/makeversiondat.bat
+++ src/xbeachlibrary/includes/version/makeversiondat.bat
@@ -3,9 +3,16 @@
 SET exe=C:\Program Files\TortoiseSVN\bin\SubWCRev.exe
 SET ok=0
 
-IF EXIST "%exe%" (
-  "%exe%" "%cd%" "%cd%\includes\version\version.tpl" "%cd%\includes\version\version.dat"
-  IF ERRORLEVEL 1 (SET ok=0) ELSE (SET ok=1)
+IF NOT DEFINED RevisionNumber (
+  IF EXIST "%exe%" (
+      "%exe%" "%cd%" "%cd%\includes\version\version.tpl" "%cd%\includes\version\version.dat"
+      IF ERRORLEVEL 1 (SET ok=0) ELSE (SET ok=1)
+  )
+) ELSE (
+  echo Build_Revision = '%RevisionNumber%' > version.dat
+  echo Build_Date     = '%DATE:~3,10% %TIME:~0,8%' >> version.dat
+  echo Build_URL      = 'http://svn.oss.deltares.nl/repos/xbeach/trunk/' >> version.dat
+  SET ok=1
 )
 
 IF %ok%==0 (
