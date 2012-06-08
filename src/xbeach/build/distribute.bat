e99e3b4d500d520d1ed589b98db78cec168e023b
geer <pieter.vangeer@deltares.nl>
2012-06-08 12:59:24 +0000

    * Add 64 bit configureations for Debug, Release, MPI_Debug and MPI_Release to the visual studio projects and solution (netcdf does not work yet, because we do not have a working 64 bit version of the netcdf library ==? TODO). * Take locally installed mpich stuff prior to using the copied mpich stuff in the repos.
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@2859 86136381-33bf-4961-916e-47ea43f679af

--- src/xbeach/build/distribute.bat
+++ src/xbeach/build/distribute.bat
@@ -3,25 +3,26 @@ echo off
 rem 1: SolutionDir
 rem 2: ConfigurationName
 rem 3: TargetDir
+rem 4: platform (win32 \ x64)
 
 echo on
 
 rem clear output dir
-if exist %1\dist\win32\%2\ rmdir %1\dist\win32\%2\ /q /s
-mkdir %1\dist\win32\%2\
+if exist %1\dist\%4\%2\ rmdir %1\dist\%4\%2\ /q /s
+mkdir %1\dist\%4\%2\
 
 rem copy exe
-copy %3\*.exe %1\dist\win32\%2\
-copy %3\*.dll %1\dist\win32\%2\
+copy %3\*.exe %1\dist\%4\%2\
+copy %3\*.dll %1\dist\%4\%2\
 
 rem copy additional dll files from static output folder
-copy %1\src\xbeachlibrary\bin\static\%2\*.dll %1\dist\win32\%2\
+copy %1\src\xbeachlibrary\bin\static\%4\%2\*.dll %1\dist\%4\%2\
 
 rem copy additional dll files from static to bin folder
-copy %1\src\xbeachlibrary\bin\static\%2\*.dll %3\
+copy %1\src\xbeachlibrary\bin\static\%4\%2\*.dll %3\
 
 rem copy current manual
-copy %1\doc\manual\xbeach_manual.pdf %1\dist\win32\%2\
+copy %1\doc\manual\xbeach_manual.pdf %1\dist\%4\%2\
 
 rem copy license
-copy %1\LICENSE %1\dist\win32\%2\
+copy %1\LICENSE %1\dist\%4\%2\
--- src/xbeachlibrary/build/generate_and_copy_gen_files.bat
+++ src/xbeachlibrary/build/generate_and_copy_gen_files.bat
@@ -1,28 +1,29 @@
 echo off
 rem 1: SolutionDir
 rem 2: ConfigurationName
+rem 3: platform (win32 / x64)
 
 set type=%2
 set type=%type:netcdf_=%
 set type=%type:MPI_=%
 echo on
 
-cd %1src\makeincludes\bin
+cd %1src\makeincludes\bin\%3
 
 rem copy input files
-copy %1src\xbeachlibrary\params.F90 %1src\makeincludes\bin
-copy %1src\xbeachlibrary\*.tmpl %1src\makeincludes\bin
+copy %1src\xbeachlibrary\params.F90 %1src\makeincludes\bin\%3
+copy %1src\xbeachlibrary\*.tmpl %1src\makeincludes\bin\%3
 
 rem del intermediate output
-del %1src\makeincludes\bin\parameters.inc
+del %1src\makeincludes\bin\%3\parameters.inc
 
 rem create gen files
-%1src\makeincludes\bin\%type%\makeincludes
+%1src\makeincludes\bin\%3\%type%\makeincludes
 
 rem delete input
-del %1src\makeincludes\bin\params.F90
+del %1src\makeincludes\bin\%3\params.F90
 
 rem copy gen files to right location
-copy %1src\makeincludes\bin\*.gen %1src\xbeachlibrary\includes\genfiles\
+copy %1src\makeincludes\bin\%3\*.gen %1src\xbeachlibrary\includes\genfiles\
 
 cd %1src\xbeachlibrary\
