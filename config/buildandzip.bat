47f5c663176d671043e5dcd30172f86079be7b0a
geer <pieter.vangeer@deltares.nl>
2012-06-08 14:43:40 +0000

    improve syntax of teamcity statements
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@2870 86136381-33bf-4961-916e-47ea43f679af

--- config/buildandzip.bat
+++ config/buildandzip.bat
@@ -6,12 +6,10 @@ echo VS version		: %3
 
 echo ##teamcity[progressMessage 'Removing old builds']
 IF EXIST "%1\dist\win32" (
-rmdir "%1\dist\win32" \s
-mkdir "%1\dist\win32"
+rmdir "%1\dist\win32\*.*" \s
 )
 IF EXIST "%1\dist\x64" (
-rmdir "%1\dist\x64" \s
-mkdir "%1\dist\x64"
+rmdir "%1\dist\x64\*.*" \s
 )
 
 IF %3==VS2010 GOTO VS2010
@@ -30,19 +28,19 @@ echo devenv		: %devenv_path%
 IF EXIST build.log del build.log
 echo on
 echo clean build
-echo ##teamcity[progressMessage 'Cleaning Release|Win32']
+echo ##teamcity[progressMessage 'Cleaning Release||Win32']
 %devenv_path%\devenv.exe "%1\%2" /Clean "Release|Win32" /Project xbeach /Out clean_release.log
 type clean_release.log
-echo ##teamcity[progressMessage 'Cleaning netcdf_Release|Win32']
+echo ##teamcity[progressMessage 'Cleaning netcdf_Release||Win32']
 %devenv_path%\devenv.exe "%1\%2" /Clean "netcdf_Release|Win32" /Project xbeach /Out clean_netcdf.log
 type clean_netcdf.log
-echo ##teamcity[progressMessage 'Cleaning MPI_Release|Win32']
+echo ##teamcity[progressMessage 'Cleaning MPI_Release||Win32']
 %devenv_path%\devenv.exe "%1\%2" /Clean "MPI_Release|Win32" /Project xbeach /Out clean_mpi.log
 type clean_mpi.log
-echo ##teamcity[progressMessage 'Cleaning Release|x64']
+echo ##teamcity[progressMessage 'Cleaning Release||x64']
 %devenv_path%\devenv.exe "%1\%2" /Clean "Release|x64" /Project xbeach /Out clean_release_x64.log
 type clean_release_x64.log
-echo ##teamcity[progressMessage 'Cleaning MPI_Release|x64']
+echo ##teamcity[progressMessage 'Cleaning MPI_Release||x64']
 %devenv_path%\devenv.exe "%1\%2" /Clean "MPI_Release|x64" /Project xbeach /Out clean_mpi.log
 type clean_mpi.log
 
@@ -54,19 +52,19 @@ EXIT /B 1
 :BUILD
 echo on
 echo build and zip xbeach executables
-echo ##teamcity[progressMessage 'Building Release|Win32']
+echo ##teamcity[progressMessage 'Building Release||Win32']
 %devenv_path%\devenv.exe "%1\%2" /Build "Release|Win32" /Project xbeach /Out build_exe.log
 type build_exe.log
-echo ##teamcity[progressMessage 'Building netcdf_Release|Win32']
+echo ##teamcity[progressMessage 'Building netcdf_Release||Win32']
 %devenv_path%\devenv.exe "%1\%2" /Build "netcdf_Release|Win32" /Project xbeach /Out build_netcdf.log
 type build_netcdf.log
-echo ##teamcity[progressMessage 'Building MPI_Release|Win32']
+echo ##teamcity[progressMessage 'Building MPI_Release||Win32']
 %devenv_path%\devenv.exe "%1\%2" /Build "MPI_Release|Win32" /Project xbeach /Out build_mpi.log
 type build_mpi.log
-echo ##teamcity[progressMessage 'Building Release|x64']
+echo ##teamcity[progressMessage 'Building Release||x64']
 %devenv_path%\devenv.exe "%1\%2" /Build "Release|x64" /Project xbeach /Out build_release_x64.log
 type build_release_x64.log
-echo ##teamcity[progressMessage 'Building MPI_Release|x64']
+echo ##teamcity[progressMessage 'Building MPI_Release||x64']
 %devenv_path%\devenv.exe "%1\%2" /Build "MPI_Release|x64" /Project xbeach /Out build_mpi_x64.log
 type build_mpi_x64.log
 echo off
