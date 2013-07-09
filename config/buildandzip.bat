30f57170b4e4453341d1a1c30eaa6c4449327b93
geer <pieter.vangeer@deltares.nl>
2013-07-09 14:26:39 +0000

    revert building one zip
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@3097 86136381-33bf-4961-916e-47ea43f679af

--- config/buildandzip.bat
+++ config/buildandzip.bat
@@ -30,33 +30,31 @@ echo devenv		: %devenv_path%
 IF EXIST build.log del build.log
 echo on
 echo clean build
-rem echo ##teamcity[progressMessage 'Cleaning Release||Win32']
-rem %devenv_path%\devenv.exe "%1\%2" /Clean "Release|Win32" /Project xbeach /Out clean_release.log
-rem type clean_release.log
-
+echo ##teamcity[progressMessage 'Cleaning Release||Win32']
+%devenv_path%\devenv.exe "%1\%2" /Clean "Release|Win32" /Project xbeach /Out clean_release.log
+type clean_release.log
 echo ##teamcity[progressMessage 'Cleaning netcdf_Release||Win32']
 %devenv_path%\devenv.exe "%1\%2" /Clean "netcdf_Release|Win32" /Project xbeach /Out clean_netcdf.log
 type clean_netcdf.log
+echo ##teamcity[progressMessage 'Cleaning MPI_Release||Win32']
+%devenv_path%\devenv.exe "%1\%2" /Clean "MPI_Release|Win32" /Project xbeach /Out clean_mpi.log
+type clean_mpi.log
+echo ##teamcity[progressMessage 'Cleaning MPI_netcdf_Release||Win32']
+%devenv_path%\devenv.exe "%1\%2" /Clean "MPI_netcdf_Release|Win32" /Project xbeach /Out clean_mpi_netcdf.log
+type clean_mpi_netcdf.log
 
-rem echo ##teamcity[progressMessage 'Cleaning MPI_Release||Win32']
-rem %devenv_path%\devenv.exe "%1\%2" /Clean "MPI_Release|Win32" /Project xbeach /Out clean_mpi.log
-rem type clean_mpi.log
-rem echo ##teamcity[progressMessage 'Cleaning MPI_netcdf_Release||Win32']
-rem %devenv_path%\devenv.exe "%1\%2" /Clean "MPI_netcdf_Release|Win32" /Project xbeach /Out clean_mpi_netcdf.log
-rem type clean_mpi_netcdf.log
-
-rem echo ##teamcity[progressMessage 'Cleaning Release||x64']
-rem %devenv_path%\devenv.exe "%1\%2" /Clean "Release|x64" /Project xbeach /Out clean_release_x64.log
-rem type clean_release_x64.log
-rem echo ##teamcity[progressMessage 'Cleaning netcdf_Release||x64']
-rem %devenv_path%\devenv.exe "%1\%2" /Clean "netcdf_Release|x64" /Project xbeach /Out clean_netcdf_x64.log
-rem type clean_netcdf_x64.log
-rem echo ##teamcity[progressMessage 'Cleaning MPI_Release||x64']
-rem %devenv_path%\devenv.exe "%1\%2" /Clean "MPI_Release|x64" /Project xbeach /Out clean_mpi_x64.log
-rem type clean_mpi_x64.log
-rem echo ##teamcity[progressMessage 'Cleaning MPI_netcdf_Release||x64']
-rem %devenv_path%\devenv.exe "%1\%2" /Clean "MPI_netcdf_Release|x64" /Project xbeach /Out clean_mpi_netcdf_x64.log
-rem type clean_mpi_netcdf_x64.log
+echo ##teamcity[progressMessage 'Cleaning Release||x64']
+%devenv_path%\devenv.exe "%1\%2" /Clean "Release|x64" /Project xbeach /Out clean_release_x64.log
+type clean_release_x64.log
+echo ##teamcity[progressMessage 'Cleaning netcdf_Release||x64']
+%devenv_path%\devenv.exe "%1\%2" /Clean "netcdf_Release|x64" /Project xbeach /Out clean_netcdf_x64.log
+type clean_netcdf_x64.log
+echo ##teamcity[progressMessage 'Cleaning MPI_Release||x64']
+%devenv_path%\devenv.exe "%1\%2" /Clean "MPI_Release|x64" /Project xbeach /Out clean_mpi_x64.log
+type clean_mpi_x64.log
+echo ##teamcity[progressMessage 'Cleaning MPI_netcdf_Release||x64']
+%devenv_path%\devenv.exe "%1\%2" /Clean "MPI_netcdf_Release|x64" /Project xbeach /Out clean_mpi_netcdf_x64.log
+type clean_mpi_netcdf_x64.log
 
 echo off
 IF %ERRORLEVEL% == 0 GOTO BUILD
@@ -67,32 +65,30 @@ EXIT /B 1
 echo on
 echo build and zip xbeach executables
 echo ##teamcity[progressMessage 'Building Release||Win32']
-rem %devenv_path%\devenv.exe "%1\%2" /Build "Release|Win32" /Project xbeach /Out build_exe.log
-rem type build_release.log
-
+%devenv_path%\devenv.exe "%1\%2" /Build "Release|Win32" /Project xbeach /Out build_exe.log
+type build_release.log
 echo ##teamcity[progressMessage 'Building netcdf_Release||Win32']
 %devenv_path%\devenv.exe "%1\%2" /Build "netcdf_Release|Win32" /Project xbeach /Out build_netcdf.log
 type build_netcdf.log
+echo ##teamcity[progressMessage 'Building MPI_Release||Win32']
+%devenv_path%\devenv.exe "%1\%2" /Build "MPI_Release|Win32" /Project xbeach /Out build_mpi.log
+type build_mpi.log
+echo ##teamcity[progressMessage 'Building MPI_netcdf_Release||Win32']
+%devenv_path%\devenv.exe "%1\%2" /Build "MPI_netcdf_Release|Win32" /Project xbeach /Out build_mpi_netcdf.log
+type build_mpi_netcdf.log
 
-rem echo ##teamcity[progressMessage 'Building MPI_Release||Win32']
-rem %devenv_path%\devenv.exe "%1\%2" /Build "MPI_Release|Win32" /Project xbeach /Out build_mpi.log
-rem type build_mpi.log
-rem echo ##teamcity[progressMessage 'Building MPI_netcdf_Release||Win32']
-rem %devenv_path%\devenv.exe "%1\%2" /Build "MPI_netcdf_Release|Win32" /Project xbeach /Out build_mpi_netcdf.log
-rem type build_mpi_netcdf.log
-
-rem echo ##teamcity[progressMessage 'Building Release||x64']
-rem %devenv_path%\devenv.exe "%1\%2" /Build "Release|x64" /Project xbeach /Out build_release_x64.log
-rem type build_release_x64.log
-rem echo ##teamcity[progressMessage 'Building netcdf_Release||x64']
-rem %devenv_path%\devenv.exe "%1\%2" /Build "netcdf_Release|x64" /Project xbeach /Out build_netcdf_x64.log
-rem type build_netcdf_x64.log
-rem echo ##teamcity[progressMessage 'Building MPI_Release||x64']
-rem %devenv_path%\devenv.exe "%1\%2" /Build "MPI_Release|x64" /Project xbeach /Out build_mpi_x64.log
-rem type build_mpi_x64.log
-rem echo ##teamcity[progressMessage 'Building MPI_netcdf_Release||x64']
-rem %devenv_path%\devenv.exe "%1\%2" /Build "MPI_netcdf_Release|x64" /Project xbeach /Out build_mpi_netcdf_x64.log
-rem type build_mpi_netcdf_x64.log
+echo ##teamcity[progressMessage 'Building Release||x64']
+%devenv_path%\devenv.exe "%1\%2" /Build "Release|x64" /Project xbeach /Out build_release_x64.log
+type build_release_x64.log
+echo ##teamcity[progressMessage 'Building netcdf_Release||x64']
+%devenv_path%\devenv.exe "%1\%2" /Build "netcdf_Release|x64" /Project xbeach /Out build_netcdf_x64.log
+type build_netcdf_x64.log
+echo ##teamcity[progressMessage 'Building MPI_Release||x64']
+%devenv_path%\devenv.exe "%1\%2" /Build "MPI_Release|x64" /Project xbeach /Out build_mpi_x64.log
+type build_mpi_x64.log
+echo ##teamcity[progressMessage 'Building MPI_netcdf_Release||x64']
+%devenv_path%\devenv.exe "%1\%2" /Build "MPI_netcdf_Release|x64" /Project xbeach /Out build_mpi_netcdf_x64.log
+type build_mpi_netcdf_x64.log
 
 echo off
 IF %ERRORLEVEL% == 0 GOTO END
