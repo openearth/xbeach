1a29be9e9af7b7e0359e99d7648f06057f0b4ccd
geer <pieter.vangeer@deltares.nl>
2013-07-10 20:05:23 +0000

    Also built dynamic libraries on teamcity (and publish them to the dist folder automatically)
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@3100 86136381-33bf-4961-916e-47ea43f679af

---
+++ src/xbeachlibrary/build/distribute.bat
@@ -0,0 +1,22 @@
+echo off
+
+rem read command line parameters
+set SolutionDir=%1
+set ConfigurationName=%2
+set TargetDir=%3
+set Platform=%4
+
+rem replace quotes
+set SolutionDir=%SolutionDir:"=%
+set ConfigurationName=%ConfigurationName:"=%
+set TargetDir=%TargetDir:"=%
+set Platform=%Platform:"=%
+
+echo on
+
+rem clear output dir
+if exist "%SolutionDir%\dist\%Platform%\%ConfigurationName%\XBeach.Library.dll" del "%SolutionDir%\dist\%Platform%\%ConfigurationName%\XBeach.Library.dll" /q /f
+if not exist "%SolutionDir%\dist\%Platform%\%ConfigurationName%\" mkdir "%SolutionDir%\dist\%Platform%\%ConfigurationName%\"
+
+rem copy additional dll files from static to bin folder
+copy "%SolutionDir%\src\xbeachlibrary\bin\dynamic\%Platform%\%ConfigurationName%\xbeachlibrary_dynamic.dll" "%SolutionDir%\dist\%Platform%\%ConfigurationName%\XBeach.Library.dll"
