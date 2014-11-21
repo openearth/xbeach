84de448a13c49c11412fa3a53b9dbe8217aad8e3
geer <pieter.vangeer@deltares.nl>
2014-11-21 15:55:58 +0000

    Another try
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@4225 86136381-33bf-4961-916e-47ea43f679af

--- config/teamcity-move.bat
+++ config/teamcity-move.bat
@@ -1,10 +1,1 @@
-
-echo 'map p-drive'
-echo %map_drive%
-%map_drive%
-
-echo y | p:\1002266-xbeach\teamcity\plink.exe xbeach@p-h5devux -pw xb3@ch123 "cp -r -f /p/1002266-xbeach/teamcity/temp_build_dir/trunk /opt/xbeach/trunk_gcc_4.9.1_1.8.1_HEAD/ && chmod 775 /opt/xbeach/trunk_gcc_4.9.1_1.8.1_HEAD/"
-
-echo 'unmap p-drive'
-echo %unmap_drive%
-%unmap_drive%
+echo y | plink.exe xbeach@p-h5devux -pw xb3@ch123 "cp -r -f /p/1002266-xbeach/teamcity/temp_build_dir/trunk /opt/xbeach/trunk_gcc_4.9.1_1.8.1_HEAD/ && chmod 775 /opt/xbeach/trunk_gcc_4.9.1_1.8.1_HEAD/"
