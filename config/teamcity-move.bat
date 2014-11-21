be087b639fe5093333ccc5a063dd55d5ef21e4ad
geer <pieter.vangeer@deltares.nl>
2014-11-21 15:59:49 +0000

    18
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@4226 86136381-33bf-4961-916e-47ea43f679af

--- config/teamcity-move.bat
+++ config/teamcity-move.bat
@@ -1,1 +1,1 @@
-echo y | plink.exe xbeach@p-h5devux -pw xb3@ch123 "cp -r -f /p/1002266-xbeach/teamcity/temp_build_dir/trunk /opt/xbeach/trunk_gcc_4.9.1_1.8.1_HEAD/ && chmod 775 /opt/xbeach/trunk_gcc_4.9.1_1.8.1_HEAD/"
+echo y | plink.exe xbeach@p-h5devux -pw pass "cp -r -f /p/1002266-xbeach/teamcity/temp_build_dir/trunk /opt/xbeach/trunk_gcc_4.9.1_1.8.1_HEAD/ && chmod 775 /opt/xbeach/trunk_gcc_4.9.1_1.8.1_HEAD/"
