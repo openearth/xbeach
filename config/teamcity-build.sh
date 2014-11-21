bf362df9ab3a23456d8285911092f76cc6a074c1
geer <pieter.vangeer@deltares.nl>
2014-11-21 11:05:17 +0000

    Try without mpi
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@4218 86136381-33bf-4961-916e-47ea43f679af

--- config/teamcity-build.sh
+++ config/teamcity-build.sh
@@ -5,9 +5,8 @@ export MODULEPATH=$MODULEPATH:/opt/modules
 module load gcc/4.9.1
 module load hdf5/1.8.13_gcc_4.9.1
 module load netcdf/v4.3.2_v4.4.0_gcc_4.9.1
-module load /opt/xbeach/openmpi/1.8.1_gcc_4.9.1
 
 ./autogen.sh
 
-FCFLAGS="-mtune=corei7-avx -funroll-loops --param max-unroll-times=4 -O3 -ffast-math" ./configure  --with-netcdf --with-mpi 
+FCFLAGS="-mtune=corei7-avx -funroll-loops --param max-unroll-times=4 -O3 -ffast-math" ./configure  --with-netcdf
 make
