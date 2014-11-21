5ebe765b4c03f05f39d1978d43e70a4f558afdc7
geer <pieter.vangeer@deltares.nl>
2014-11-21 12:24:48 +0000

    temporary hard reference to openmpi dependency (use module when available )
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@4219 86136381-33bf-4961-916e-47ea43f679af

--- config/teamcity-build.sh
+++ config/teamcity-build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 . /etc/profile
 export MODULEPATH=$MODULEPATH:/opt/modules
+export LD_LIBRARY_PATH=/opt/openmpi/1.8.1_gcc_4.9.1/lib:$LD_LIBRARY_PATH
+export PATH=/opt/openmpi/1.8.1_gcc_4.9.1/bin:$PATH
 
 module load gcc/4.9.1
 module load hdf5/1.8.13_gcc_4.9.1
 module load netcdf/v4.3.2_v4.4.0_gcc_4.9.1
+
 
 ./autogen.sh
 
