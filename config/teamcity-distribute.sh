28d1ae1699498eddb57daddf09b6e820b0d5989f
geer <pieter.vangeer@deltares.nl>
2014-11-25 08:13:23 +0000

    TODO: Change permissions of destination folder
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@4237 86136381-33bf-4961-916e-47ea43f679af

--- config/teamcity-distribute.sh
+++ config/teamcity-distribute.sh
@@ -4,5 +4,6 @@
 chmod 775 /opt/teamcity/work/XBeach_unix/install
 
 cd /opt/teamcity/work/XBeach_unix/install
-scp -r -i $HOME/xbeach/xbeach-key bin lib geer@h5:tcxbeach
+
+scp -r -i $HOME/xbeach/xbeach-key bin lib geer@h5:/opt/xbeach/trunk_gcc_4.9.1_1.8.1_HEAD
 # share trunk 
