43aa20d79dae9fbee0687a95335598f2e1c91236
bieman <joost.denbieman@deltares.nl>
2014-12-09 10:21:46 +0000

    copy to correct location
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@4283 86136381-33bf-4961-916e-47ea43f679af

--- config/teamcity-distribute.sh
+++ config/teamcity-distribute.sh
@@ -5,5 +5,5 @@ chmod 775 /opt/teamcity/work/XBeach_unix/install
 
 cd /opt/teamcity/work/XBeach_unix/install
 
-scp -r -i $HOME/xbeach/xbeach-key bin lib geer@h5:/opt/xbeach/trunk_gcc_4.9.1_1.8.1_HEAD
+scp -r -i $HOME/xbeach/xbeach-key bin lib geer@h5:/opt/xbeach/trunk_gcc_4.9.2_1.8.3_HEAD
 # share trunk 
