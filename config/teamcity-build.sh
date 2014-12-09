5281c420f4be6884e78f9245250a1c046b064aeb
bieman <joost.denbieman@deltares.nl>
2014-12-09 10:21:18 +0000

    compile with mpi on build server
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@4282 86136381-33bf-4961-916e-47ea43f679af

--- config/teamcity-build.sh
+++ config/teamcity-build.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 . /etc/profile
 export MODULEPATH=$MODULEPATH:/opt/modules
-export LD_LIBRARY_PATH=/opt/openmpi/1.8.1_gcc_4.9.1/lib:$LD_LIBRARY_PATH
-export PATH=/opt/openmpi/1.8.1_gcc_4.9.1/bin:$PATH
 
-module load gcc/4.9.1
-module load hdf5/1.8.13_gcc_4.9.1
-module load netcdf/v4.3.2_v4.4.0_gcc_4.9.1
+module load gcc/4.9.2
+module load hdf5/1.8.14_gcc_4.9.2
+module load netcdf/v4.3.2_v4.4.0_gcc_4.9.2
+module load openmpi/1.8.1_gcc_4.9.2
 
 ./autogen.sh
 
 mkdir -p /opt/teamcity/work/XBeach_unix/install
 
-FCFLAGS="-mtune=corei7-avx -funroll-loops --param max-unroll-times=4 -O3 -ffast-math" ./configure  --with-netcdf --prefix="/opt/teamcity/work/XBeach_unix/install"
+FCFLAGS="-mtune=corei7-avx -funroll-loops --param max-unroll-times=4 -O3 -ffast-math" ./configure  --with-netcdf --with-mpi --prefix="/opt/teamcity/work/XBeach_unix/install"
 
 make
 make install
