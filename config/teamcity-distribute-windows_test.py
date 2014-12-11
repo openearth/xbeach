da2fefa7da6c98f54ef6b69dd64d0aaefdd589a3
geer <pieter.vangeer@deltares.nl>
2014-12-11 10:53:20 +0000

    test
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@4298 86136381-33bf-4961-916e-47ea43f679af

--- config/teamcity-distribute-windows_test.py
+++ config/teamcity-distribute-windows_test.py
@@ -14,31 +14,29 @@ def distribute(scptarget,revision,bindir,workdir):
 	files = glob.glob(pattern)
 
 	ln = ''
-	
-	# file = files[0]
-	#for file in files:
-	#	nameparts = file.replace('\\','/').replace(bindir.replace('\\','/'),"").split('/')
-	#	platform = nameparts[0]
-	#	configuration = nameparts[1]
-	#	filename = nameparts[2]
+	for file in files:
+		nameparts = file.replace('\\','/').replace(bindir.replace('\\','/'),"").split('/')
+		platform = nameparts[0]
+		configuration = nameparts[1]
+		filename = nameparts[2]
 		
-	#	# Copy file to bin dir (with correct name)
-	#	newname = "xbeach_" + str(revision) + "_" + platform + "_" + releases[configuration][1] + ".zip"
-	#	scpcopyfile(workdir, file, scptarget + "bin/" + newname)
+		# Copy file to bin dir (with correct name)
+		newname = "xbeach_" + str(revision) + "_" + platform + "_" + releases[configuration][1] + ".zip"
+		scpcopyfile(workdir.replace('\\','/')+'/', file, scptarget + "bin/" + newname)
 		
-	#	# fill template list string
-	#	label = "XBeach rev. %d %s (%s)" % (revision,platform,releases[configuration][0])
-	#	ln += '<li><a href="bin/%s">%s</a></li>%s' % (newname,label,os.linesep)
+		# fill template list string
+		label = "XBeach rev. %d %s (%s)" % (revision,platform,releases[configuration][0])
+		ln += '<li><a href="bin/%s">%s</a></li>%s' % (newname,label,os.linesep)
 		
 	# string replace in tmp file
-	workhtml = workdir + "index_bin.html"
-	#with open(workhtml, "wt") as fout:
-	#	with open(workdir + "index_bin.html.tmpl", "rt") as fin:
-	#		for line in fin:
-	#		    fout.write(line.replace('${list}', ln))
+	workhtml = workdir.replace('\\','/') + "/index_bin.html"
+	with open(workhtml, "wt") as fout:
+		with open(workdir.replace('\\','/') + "/index_bin.html.tmpl", "rt") as fin:
+			for line in fin:
+			    fout.write(line.replace('${list}', ln))
 	
 	# Copy all zips and html to oss site
-	scpcopyfile(workdir, workhtml, scptarget + "index_bin.html")
+	scpcopyfile(workdir.replace('\\','/'), workhtml, scptarget + "index_bin.html")
 
 def scpcopyfile(workdir, source,destination):
 	import subprocess
