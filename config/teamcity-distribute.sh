c9d1e5e0f688ecac3c1b8e6e2caa054de8dca41d
geer <pieter.vangeer@deltares.nl>
2015-03-31 12:23:35 +0000

    Fix copy step?
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@4438 86136381-33bf-4961-916e-47ea43f679af

--- config/teamcity-distribute.sh
+++ config/teamcity-distribute.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 . /etc/profile
 
+mkdir -p $HOME/xbeach
+printenv | grep xbeach_ssh_key_private | tr -d "'" | sed 's/\\n/\n/g' > $HOME/xbeach/xbeach-key
+printenv | grep xbeach_ssh_key_public | tr -d "'" | sed 's/\\n/\n/g' > $HOME/xbeach/xbeach-key.pub
+
 chmod 775 /opt/teamcity/work/XBeach_unix/install
 
 cd /opt/teamcity/work/XBeach_unix/install
 
 scp -r -i $HOME/xbeach/xbeach-key bin lib geer@h5:/opt/xbeach/trunk_gcc_4.9.2_1.8.3_HEAD
+
 # share trunk 
