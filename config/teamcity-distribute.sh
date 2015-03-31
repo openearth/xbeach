965d45bd78272ae34818a0c8be6583fc13267b62
geer <pieter.vangeer@deltares.nl>
2015-03-31 14:20:39 +0000

    Remove generation of key file
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@4440 86136381-33bf-4961-916e-47ea43f679af

--- config/teamcity-distribute.sh
+++ config/teamcity-distribute.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 . /etc/profile
 
-printenv | grep xbeach_ssh_key_public | tr -d "'" | sed 's/\\n/\n/g'
-
-mkdir -p $HOME/xbeach
-printenv | grep xbeach_ssh_key_private | tr -d "'" | sed 's/\\n/\n/g' > $HOME/xbeach/xbeach-key
-printenv | grep xbeach_ssh_key_public | tr -d "'" | sed 's/\\n/\n/g' > $HOME/xbeach/xbeach-key.pub
-
 chmod 775 /opt/teamcity/work/XBeach_unix/install
 
 cd /opt/teamcity/work/XBeach_unix/install
