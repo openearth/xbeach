3fd36b6311a5473817381c182224264dfbcfd1f9
keesnederhoff <kees.nederhoff@deltares.nl>
2015-04-22 08:55:30 +0000

    Wind correction
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@4566 86136381-33bf-4961-916e-47ea43f679af

--- src/xbeachlibrary/initialize.F90
+++ src/xbeachlibrary/initialize.F90
@@ -1034,6 +1034,178 @@ contains
          s%zs0 = s%zs01
       endif
 
+<<<<<<< .mine
+    ! R+L: Why are these variable reinitialise? 
+    s%dzsdt=0.d0
+    s%dzsdx=0.d0
+    s%dzsdy=0.d0
+    s%dzbdt=0.d0
+    s%uu=0.d0
+    s%u=0.d0
+    s%vv=0.d0
+    s%v=0.d0
+    s%vu=0.d0
+    s%uv=0.d0
+    s%ueu=0.d0
+    s%vev=0.d0
+    s%qx=0.d0
+    s%qy=0.d0
+    s%sedero=0.d0
+    s%vmagu=0.d0
+    s%vmagv=0.d0
+    s%vmageu=0.d0
+    s%vmagev=0.d0
+    s%taubx=0.d0
+    s%tauby=0.d0
+    s%maxzs=-999.d0
+    s%minzs=999.d0
+    where(s%zs>s%zb+par%eps)
+       s%wetz=1
+       s%wetu=1
+       s%wetv=1
+    elsewhere
+       s%wetz=0
+       s%wetu=0
+       s%wetv=0
+    endwhere
+    !
+    ! Start with initial velocities based on balance between water level gradient
+    ! and bed friction term
+    if (par%hotstartflow==1) then
+       call writelog('ls','','Calculating stationary flow field for initial condition')
+       ! water level gradients
+       do j=1,s%ny+1
+          do i=2,s%nx
+             s%dzsdx(i,j)=(s%zs0(i+1,j)-s%zs0(i,j))/s%dsu(i,j)
+          end do
+       end do
+       do j=1,s%ny ! Dano need to get correct slope on boundary y=0
+          do i=1,s%nx+1
+             s%dzsdy(i,j)=(s%zs0(i,j+1)-s%zs0(i,j))/s%dnv(i,j)
+          end do
+       end do
+       ! Water depth in u,v-points mean for wind forcing
+       do j=1,s%ny+1
+          do i=1,s%nx+1 !Ap
+             s%hum(i,j)=max(.5d0*(s%hh(i,j)+s%hh(min(s%nx,i)+1,j)),par%eps) 
+          end do
+       end do
+       do j=1,s%ny+1
+          do i=1,s%nx+1
+             s%hvm(i,j)=max(.5d0*(s%hh(i,j)+s%hh(i,min(s%ny,j)+1)),par%eps)
+          end do
+       end do
+       ! residual error
+       flowerr = huge(0.d0)
+       vmagvold = 0.d0
+       vmaguold = 0.d0
+       do while (flowerr > 0.00001d0)
+          !
+          ! Balance in longshore
+          vmagvold=0.5d0*(vmagvold+sqrt(s%uv**2+s%vv**2))   ! mean needed for convergence
+          ! solve v-balance of pressure gradient, wind forcing and bed friction
+          where (s%wetv==1)
+             s%vv = s%hv/s%cf/max(vmagvold,0.000001d0) &
+                  *(-par%g*s%dzsdy+par%rhoa*par%Cd*s%windnv*sqrt(s%windsu**2+s%windnv**2)/(par%rho*s%hvm))  ! Kees: wind correction 
+          elsewhere
+             s%vv = 0.d0
+          endwhere
+          ! update vmagev
+          ! u velocity in v points
+          if (s%ny>0) then
+             s%uv(2:s%nx,1:s%ny)= .25d0*(s%uu(1:s%nx-1,1:s%ny)+s%uu(2:s%nx,1:s%ny)+ &
+                  s%uu(1:s%nx-1,2:s%ny+1)+s%uu(2:s%nx,2:s%ny+1))
+             ! boundaries?
+             ! wwvv and what about uv(:,1) ?
+             if(xmpi_isright) then
+                s%uv(:,s%ny+1) = s%uv(:,s%ny)
+             endif
+          else
+             s%uv(2:s%nx,1)= .5d0*(s%uu(1:s%nx-1,1)+s%uu(2:s%nx,1))
+          endif !s%ny>0
+          s%vmagev = sqrt(s%uv**2+s%vv**2)
+          !
+          ! Balance in cross shore
+          vmaguold= 0.5d0*(vmaguold+sqrt(s%uu**2+s%vu**2)) ! mean needed for convergence
+          ! Solve balance of forces
+          where (s%wetu==1)
+             s%uu = s%hu/s%cf/max(vmaguold,0.000001d0) &
+                  *(-par%g*s%dzsdx+par%rhoa*par%Cd*s%windsu*sqrt(s%windsu**2+s%windnv**2)/(par%rho*s%hum)) ! Kees: wind correction 
+          elsewhere
+             s%uu = 0.d0
+          endwhere
+          ! update vmageu
+          if (s%ny>0) then
+             s%vu(1:s%nx,2:s%ny)= 0.25d0*(s%vv(1:s%nx,1:s%ny-1)+s%vv(1:s%nx,2:s%ny)+ &
+                  s%vv(2:s%nx+1,1:s%ny-1)+s%vv(2:s%nx+1,2:s%ny))
+             if(xmpi_isleft) then
+                s%vu(:,1) = s%vu(:,2)
+             endif
+             if(xmpi_isright) then
+                s%vu(:,s%ny+1) = s%vu(:,s%ny)
+             endif
+          else 
+             s%vu(1:s%nx,1)= 0.5d0*(s%vv(1:s%nx,1)+s%vv(2:s%nx+1,1))
+          endif !s%ny>0
+          s%vmageu = sqrt(s%uu**2+s%vu**2)
+          !
+          ! Check residual error
+          flowerr = max(maxval(abs(s%vmagev-vmagvold)),maxval(abs(s%vmageu-vmaguold)))          
+       enddo
+       ! calculate all derivatives
+       s%ueu=s%uu
+       s%vev=s%vv
+       s%qx=s%uu*s%hu
+       s%qy=s%vv*s%hv
+       s%vmagu=s%vmageu
+       s%vmagv=s%vmagev
+       s%u(2:s%nx,:)=0.5d0*(s%uu(1:s%nx-1,:)+s%uu(2:s%nx,:))
+       if(xmpi_istop) then
+          s%u(1,:)=s%uu(1,:)
+       endif
+       if(xmpi_isbot) then
+          s%u(s%nx+1,:)=s%u(s%nx,:)
+       endif
+       if (s%ny>0) then
+          s%v(:,2:s%ny)=0.5d0*(s%vv(:,1:s%ny-1)+s%vv(:,2:s%ny))
+          if(xmpi_isleft) then
+             s%v(:,1)=s%vv(:,1)
+          endif
+          if(xmpi_isright) then
+             s%v(:,s%ny+1)=s%v(:,s%ny)
+          endif
+          s%v(s%nx+1,:)=s%v(s%nx,:)
+       else ! Dano
+          s%v=s%vv
+       endif !s%ny>0
+    endif
+    !
+    ! Initialize for tide instant boundary condition
+    !
+    if (par%tidetype==TIDETYPE_INSTANT) then
+       ! RJ: 22-09-2010
+       ! Check for whole domain whether a grid cell should be associated with
+       ! 1) offshore tide and surge
+       ! 2) bay tide and surge
+       ! 3) no tide and surge
+       ! 4) weighted tide and surge (for completely wet arrays)
+       ! relative weight of offshore boundary and bay boundary for each grid point is stored in zs0fac 
+       !
+       s%zs0fac = 0.d0
+       do j = 1,s%ny+1 
+          offshoreregime = .true.   
+          indoff = s%nx+1 ! ind of last point (starting at offshore boundary) that should be associated with offshore boundary
+          indbay = 1      ! ind of first point (starting at offshore boundary) that should be associated with bay boundary
+          do i = 1,s%nx+1
+             if (offshoreregime .and. s%wetz(i,j)==0) then
+                indoff = max(i-1,1)
+                offshoreregime = .false.
+             endif
+             if (s%wetz(i,j)==0 .and. s%wetz(min(i+1,s%nx+1),j)==1) then
+                indbay = min(i+1,s%nx+1)
+             endif
+          enddo
+=======
       inquire(file=par%zsinitfile,exist=exists)
       if (exists) then
          open(723,file=par%zsinitfile)
@@ -1045,6 +1217,7 @@ contains
          enddo
          close(723)
       endif
+>>>>>>> .r4565
 
       inquire(file=par%bedfricfile,exist=exists)
       if ((exists)) then
@@ -1184,10 +1357,10 @@ contains
             ! Balance in cross shore
             vmaguold= 0.5d0*(vmaguold+sqrt(s%uu**2+s%vu**2)) ! mean needed for convergence
             ! Solve balance of forces
-            where (s%wetu==1)
-               s%uu = s%hu/s%cf/max(vmaguold,0.000001d0) &
-               *(-par%g*s%dzsdx+par%rhoa*par%Cd*s%windsu**2/(par%rho*s%hum))
-            elsewhere
+              where (s%wetu==1)
+                 s%uu = s%hu/s%cf/max(vmaguold,0.000001d0) &
+                      *(-par%g*s%dzsdx+par%rhoa*par%Cd*s%windsu*sqrt(s%windsu**2+s%windnv**2)/(par%rho*s%hum)) ! Kees: wind correction 
+              elsewhere
                s%uu = 0.d0
             endwhere
             ! update vmageu
