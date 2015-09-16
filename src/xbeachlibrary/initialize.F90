ada20a04b3c76e4595fff8c33fd5215ba18024a6
rooijen <arnold.vanrooijen@deltares.nl>
2015-09-16 15:14:15 +0000

    Fix in case total sum of sediment fractions ~= 1 (fix for Melike)
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@4777 86136381-33bf-4961-916e-47ea43f679af

--- src/xbeachlibrary/initialize.F90
+++ src/xbeachlibrary/initialize.F90
@@ -1461,9 +1461,9 @@ contains
                do i=1,s%nx+1 !Jaap instead of 2:s%nx
 
                   tempr=sum(s%pbbed(i,j,m,1:par%ngd))
-                  if (abs(1.d0-tempr)>0.d0) then
+                if (abs(1.d0-tempr)>0.000001d0) then
                      ! Maybe fix this warning if in combination with structures
-                     call writelog('lws','ai0ai0ai0a',' Warning: Resetting sum of sediment fractions in point (',&
+                     call writelog('lws','(a,i,a,i,a,i,a)',' Warning: Resetting sum of sediment fractions in point (',&
                      i,',',j,') layer ,',m,&
                      ' to equal unity.')
                      if (tempr<=tiny(0.d0)) then    ! In case cell has zero sediment (i.e. only hard structure)
