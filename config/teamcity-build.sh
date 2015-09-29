5a6466d4cebd7efafe3d2c17c9fbb5c524b4d2f5
bieman <joost.denbieman@deltares.nl>
2015-09-29 13:00:40 +0000

    differentiate svn checkout names (so .sh scripts need to be differentiated as well) Related to Issue XBEACH-169
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@4804 86136381-33bf-4961-916e-47ea43f679af

--- config/teamcity-build.sh
+++ config/teamcity-build.sh
@@ -14,11 +14,11 @@ make distclean
 
 ./autogen.sh
 
-mkdir -p /opt/teamcity/work/XBeach_unix/install
+mkdir -p /opt/teamcity/work/XBeach_unix_trunk/install
 
-FCFLAGS="-mtune=corei7-avx -funroll-loops --param max-unroll-times=4 -ffree-line-length-none -O3 -ffast-math" ./configure  --with-netcdf --with-mpi --prefix="/opt/teamcity/work/XBeach_unix/install"
+FCFLAGS="-mtune=corei7-avx -funroll-loops --param max-unroll-times=4 -ffree-line-length-none -O3 -ffast-math" ./configure  --with-netcdf --with-mpi --prefix="/opt/teamcity/work/XBeach_unix_trunk/install"
 
 make
 make install
 
-/usr/share/Modules/bin/createmodule.py -p "/opt/xbeach/"$XBEACH_PROJECT_ID"_gcc_4.9.2_1.8.3_HEAD" ./config/teamcity-env.sh > "/opt/teamcity/work/XBeach_unix/config/xbeach-"$XBEACH_PROJECT_ID"_gcc_4.9.2_1.8.3_HEAD"
+/usr/share/Modules/bin/createmodule.py -p "/opt/xbeach/"$XBEACH_PROJECT_ID"_gcc_4.9.2_1.8.3_HEAD" ./opt/teamcity/work/XBeach_unix_trunk/trunk/config/teamcity-env.sh > "/opt/teamcity/work/XBeach_unix_trunk/trunk/config/xbeach-"$XBEACH_PROJECT_ID"_gcc_4.9.2_1.8.3_HEAD"
--- config/teamcity-distribute.sh
+++ config/teamcity-distribute.sh
@@ -3,13 +3,13 @@
 
 chmod 775 /opt/teamcity/work/XBeach_unix/install
 
-cd /opt/teamcity/work/XBeach_unix/install
+cd /opt/teamcity/work/XBeach_unix_trunk/install
 
 copypath="/opt/xbeach/"$XBEACH_PROJECT_ID"_gcc_4.9.2_1.8.3_HEAD"
 
 scp -r -i $HOME/xbeach/xbeach-key bin lib geer@h5:$copypath
 
-cd /opt/teamcity/work/XBeach_unix/config
+cd /opt/teamcity/work/XBeach_unix_trunk/config
 
 modulepath="/opt/xbeach/"$XBEACH_PROJECT_ID"_gcc_4.9.2_1.8.3_HEAD"
 
