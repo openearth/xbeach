644a4519161ce10c03e3ea2a12874eb4968789eb
bieman <joost.denbieman@deltares.nl>
2015-09-29 21:40:20 +0000

    Related to Issue XBEACH-169
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@4810 86136381-33bf-4961-916e-47ea43f679af

--- config/teamcity-build.sh
+++ config/teamcity-build.sh
@@ -24,6 +24,8 @@ make install
 echo "/opt/xbeach/"$XBEACH_PROJECT_ID"_gcc_4.9.2_1.8.3_HEAD"
 echo "/opt/teamcity/work/XBeach_unix_trunk/trunk/config/xbeach-"$XBEACH_PROJECT_ID"_gcc_4.9.2_1.8.3_HEAD"
 
+chmod 775 /opt/teamcity/work/XBeach_unix_trunk/trunk/config/teamcity-env.sh
+
 /usr/share/Modules/bin/createmodule.py -p "/opt/xbeach/"$XBEACH_PROJECT_ID"_gcc_4.9.2_1.8.3_HEAD" ./opt/teamcity/work/XBeach_unix_trunk/trunk/config/teamcity-env.sh > "/opt/teamcity/work/XBeach_unix_trunk/trunk/config/xbeach-"$XBEACH_PROJECT_ID"_gcc_4.9.2_1.8.3_HEAD"
 
 ls -l /opt/teamcity/work/XBeach_unix_trunk/trunk/config/teamcity-env.sh
