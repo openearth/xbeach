6d4510efbd351551a8fe164a58ca58d52b00fb1a
bieman <joost.denbieman@deltares.nl>
2015-11-11 08:56:09 +0000

    write script to depend on environment parameter for cluster location
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@4878 86136381-33bf-4961-916e-47ea43f679af

--- config/teamcity-distribute.sh
+++ config/teamcity-distribute.sh
@@ -9,8 +9,8 @@ cd $SVNPATH"/install"
 
 copypath="/opt/xbeach/"$XBEACH_PROJECT_ID"_gcc_4.9.2_1.8.3_HEAD"
 
-scp -r -i $HOME/xbeach/xbeach-key bin lib geer@h5:$copypath
+scp -r -i $HOME/xbeach/xbeach-key bin lib geer@$CLUSTER_HOSTNAME:$copypath
 
 cd $SVNPATH"/trunk/config"
 
-scp -r -i $HOME/xbeach/xbeach-key "xbeach-"$XBEACH_PROJECT_ID"_gcc_4.9.2_1.8.3_HEAD" geer@h5:/opt/xbeach/modules/
+scp -r -i $HOME/xbeach/xbeach-key "xbeach-"$XBEACH_PROJECT_ID"_gcc_4.9.2_1.8.3_HEAD" geer@$CLUSTER_HOSTNAME:/opt/xbeach/modules/
