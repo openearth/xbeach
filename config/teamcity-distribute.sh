0d8ff1e1f7c6fbac954bb76c0375b0825129dc61
bieman <joost.denbieman@deltares.nl>
2016-02-04 12:33:13 +0000

    H5 is no longer operational, so distributing to it has no use
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@4922 86136381-33bf-4961-916e-47ea43f679af

--- config/teamcity-distribute.sh
+++ config/teamcity-distribute.sh
@@ -10,9 +10,7 @@ cd $SVNPATH"/install"
 copypath="/opt/xbeach/"$XBEACH_PROJECT_ID"_gcc_4.9.2_1.8.3_HEAD"
 
 scp -r -i $HOME/.ssh/id_rsa bin lib xbeach@$CLUSTER_HOSTNAME:$copypath
-scp -r -i $HOME/.ssh/id_rsa bin lib xbeach@h5:$copypath
 
 cd $SVNPATH"/trunk/config"
 
 scp -r -i $HOME/.ssh/id_rsa "xbeach-"$XBEACH_PROJECT_ID"_gcc_4.9.2_1.8.3_HEAD" xbeach@$CLUSTER_HOSTNAME:/opt/xbeach/modules/
-scp -r -i $HOME/.ssh/id_rsa "xbeach-"$XBEACH_PROJECT_ID"_gcc_4.9.2_1.8.3_HEAD" xbeach@h5:/opt/xbeach/modules/
