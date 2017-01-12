d1f9274c490d0d96e7843842e56ca304b55fe7cc
hoonhout <bas@hoonhout.com>
2017-01-12 15:46:32 +0000

    Replaced paramiko with custom SSHClient class as build agents are not properly configured
    git-svn-id: https://svn.oss.deltares.nl/repos/xbeach/trunk@5051 86136381-33bf-4961-916e-47ea43f679af

--- config/teamcity-distribute.py
+++ config/teamcity-distribute.py
@@ -1,6 +1,7 @@
 import os
 import re
 import sys
+import subprocess
 import paramiko
 import zipfile
 
@@ -9,17 +10,54 @@ PLATFORMS = ['win32', 'x64']
 CONFIGURATIONS = ['Release', 'netcdf_Release', 'MPI_Release', 'MPI_netcdf_Release']
 
 
+class SSHClient:
+    '''Dummy object to bypass paramiko'''
+
+    def __init__(self, workdir):
+        self.workdir = workdir
+        
+    
+    def set_missing_host_key_policy(self, policy):
+        self.missing_host_key_policy = policy
+        
+        
+    def connect(self, host, port=22, username=None, password=None):
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        
+        
+    def open_sftp(self):
+        return self
+        
+        
+    def put(self, src, dst):
+
+        FNULL = open(os.devnull, 'w')
+        pscp = self.workdir + '\\pscp.exe'
+    
+        args = 'echo y | %s -l %s -pw %s -r -v %s %s:%s' % (pscp, self.username, self.password, src, self.host, dst)
+        subprocess.call(args, stdout=FNULL, stderr=FNULL, shell=True)
+
+
+    def close(self):
+        pass
+    
+    
 def distribute(targetdir, revision, bindir, workdir, username, password, host='v-oss003.dlt.proteon.nl', port=22):
     '''Zip releases and upload to OSS website'''
 
     # open ssh socket
-    transport = paramiko.Transport((host, port))
-    transport.connect(username=username, password=password)
-    sftp = paramiko.SFTPClient.from_transport(transport)
+    ssh = SSHClient(workdir) #ssh = paramiko.SSHClient() # doesn't work because of buggy instalation on build agents
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(host, port=port, username=username, password=password)
+    sftp = ssh.open_sftp()
 
-    print '##teamcity[message text=\'Copying binaries\']'
-
+    print '##teamcity[message text=\'Uploading binaries\']'
+    
     # loop over configurations
+    ln = ''
     for platform in PLATFORMS:
         for configuration in CONFIGURATIONS:
 
@@ -31,34 +69,36 @@ def distribute(targetdir, revision, bindir, workdir, username, password, host='v
                 
                 # zip configuration files
                 zname = 'xbeach_%d_%s_%s.zip' % (revision, platform, cid)
+                print '##teamcity[message text=\'Creating archive %s\']' % zname
                 zpath = zip_directory_contents(cpath, zname)
 
                 # upload zip file
+                print '##teamcity[message text=\'Uploading %s\']' % zname
                 sftp.put(zpath, targetdir + 'bin/')
                 
-		# fill template list string
-		label = 'XBeach rev. %d %s (%s)' % (revision, platform, clabel)
-		ln += '<li><a href='bin/%s'>%s</a></li>%s' % (zname, label, os.linesep)
-		
-    print '##teamcity[message text=\'Finished copying binaries\']'
-	
+                # fill template list string
+                label = 'XBeach rev. %d %s (%s)' % (revision, platform, clabel)
+                ln += '<li><a href="bin/%s">%s</a></li>%s' % (zname, label, os.linesep)
+        
+    print '##teamcity[message text=\'Finished uploading binaries\']'
+    
     print '##teamcity[message text=\'Creating index html of download page\']'
 
     # string replace in tmpl file (should be mako, but not available on build agents)
     htmlfile = os.path.join(workdir, 'index_bin.html')
     tmplfile = os.path.join(workdir, 'index_bin.html.tmpl')
     with open(htmlfile, 'wt') as fout:
-	with open(tmplfile, 'rt') as fin:
-	    for line in fin:
-		fout.write(line.replace('${list}', ln))
-	        
+        with open(tmplfile, 'rt') as fin:
+            for line in fin:
+                fout.write(line.replace('${list}', ln))
+            
     # copy html to oss site
     sftp.put(htmlfile, targetdir)
 
     sftp.close()
-    transport.close()
+    ssh.close()
 
-                
+    
 def zip_directory_contents(cpath, zname):
     '''Zip entire directory and return path to resulting zip file
 
@@ -77,9 +117,11 @@ def zip_directory_contents(cpath, zname):
     '''
     
     zpath = os.path.join(cpath, zname)
-    with zipfile(zpath, 'w') as fp:
+    with zipfile.ZipFile(zpath, 'w') as fp:
         for root, dirs, files in os.walk(cpath):
             for fname in files:
+                if fname.endswith('.zip'):
+                    continue
                 fp.write(os.path.join(root, fname))
 
     return zpath
@@ -88,7 +130,7 @@ def zip_directory_contents(cpath, zname):
 def get_configuration_identifiers(configuration):
     '''Returns ID and label identifying a configuration'''
 
-    cid = re.sub('_?Release$', '').lower()
+    cid = re.sub('_?Release$', '', configuration).lower()
 
     if not cid:
         cid = 'trunk'
@@ -102,8 +144,8 @@ def get_configuration_identifiers(configuration):
 if __name__ == '__main__':
 
     if not(len(sys.argv)==6):
-	raise Exception('Wrong number of input arguments')
-	
+        raise Exception('Wrong number of input arguments')
+    
     revision = int(sys.argv[1])
     targetdir = '/htdocs/xbeach/testbed/'
     bindir = sys.argv[2]
